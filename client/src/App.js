b51f1361a6e626102366101a5a2841cb9ea399f7
TextError <savinraul1983@yahoo.com>
2019-10-24 09:20:40 +0300

    Basic pages, routes and redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './layout/navbar/Navbar';
+import Home from './pages/home/Home';
+import SignIn from './pages/signIn/SignIn';
+import Register from './pages/register/Register';
+import NotFound from './pages/notFound/NotFound';
+import Footer from './layout/footer/Footer';
 
 const App = () => {
   return (
-    <div>
-      my app
-    </div>
+    <Router>
+      <div className='app'>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/sign-in' component={SignIn} />
+          <Route exact path='/register' component={Register} />
+
+          <Route path='*' component={NotFound} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 

5931b9756931a8e20e473ce15d2bac5c7feec797
TextError <savinraul1983@yahoo.com>
2019-10-31 12:01:12 +0200

    Textarea css

--- client/src/components/common/textarea/Styled_Textarea.js
+++ client/src/components/common/textarea/Styled_Textarea.js
@@ -1,27 +1,55 @@
 import styled from 'styled-components';
 
+const shrink = () => `
+  top: -7px;
+  background: var(--mainWhite);
+  border-radius: var(--BorderRadius);
+  padding: 2px 10px;
+  font-weight: 600;
+  letter-spacing: var(--Spacing);
+  left: 12px;
+  font-size: .7em;
+  color: var(--Green);
+`
+
 const StyledTextarea = styled.div`
   position: relative;
   margin: 25px 0;
   textarea {
     width: 100%;
-    padding: 10px 10px 25px;
+    padding: 10px;
     border: 1px solid var(--Shadow);
     border-radius: var(--BorderRadius);
     overflow-y: hidden;
     resize: none;
+    transition: var(--Transition);
+    &:focus ~ .label {
+      ${shrink}
+    }
+  }
+  .label {
+    position: absolute;
+    top: 11px;
+    left: 12px;
+    color: var(--mainBlack);
+    letter-spacing: var(--Spacing);
+    text-transform: uppercase;
+    font-weight: 500;
+    transition: var(--Transition); 
+    &.shrink {
+      ${shrink}
+    }   
   }
   .badge {
     position: absolute;
-    right: 0;
-    bottom: 5px;
-    padding: 5px 5px 4px 5px;
+    right: 5px;
+    top: -13px;
+    padding: 5px 8px 4px 8px;
     margin: 5px;
-    border: 1px solid var(--Green);
-    border-radius: var(--BorderRadius);
+    background: var(--mainWhite);
   }
   .over {
-    border-color: var(--Error);
+    color: var(--Error);
   }
 `
 
--- client/src/components/common/textarea/Textarea.jsx
+++ client/src/components/common/textarea/Textarea.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import Textarea from 'react-textarea-autosize';
@@ -6,13 +6,42 @@ import Textarea from 'react-textarea-autosize';
 import StyledTextarea from './Styled_Textarea';
 
 const TextArea = ({ name, label, text, onChange, onFocus, error }) => {
+  const [state, setState] = useState({ row: 1, focus: 0 });
+  const { row, focus } = state;
+  const outsideRef = React.createRef();
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  });
+
+  const handleClickOutside = e => {
+    const { current } = outsideRef;
+    if(current === null) return null;
+    if(e.target.className !== 'ref' && !focus && !text.length < 50) return null;
+    if(e.target.className === 'ref' && focus) return null;
+    setState({ row: 1, focus: 0 })
+  };
+
+  const onFocusTextarea = () => {
+    if(row === 1) setState({ row: 3, focus: 1 });
+  }
+
   return (
-    <StyledTextarea>
-      <Textarea minRows={3} name={name} onFocus={onFocus} onChange={e => onChange(e)} value={text} />
+    <StyledTextarea focus={focus}>
+      <Textarea 
+        className='ref'
+        minRows={row} 
+        ref={outsideRef}
+        name={name} 
+        value={text}
+        onChange={e => onChange(e)} 
+        onFocus={onFocusTextarea} 
+      />
       <span className={classnames('badge', { 'over': text.length > 50 })}>{text.length}</span>
-      {/* <label className={classnames('form-input-label', { 'shrink': text || error })} >
+      <label className={classnames('label', { 'shrink': text || error })} >
         {error ? error : label}
-      </label> */}
+      </label>
     </StyledTextarea>
   )
 }
